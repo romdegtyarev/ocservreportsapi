3a6d771789480c86838130fc082fd6e0e88b36e8
Roman Degtyarev <guromka@gmail.com>
2024-09-27 13:07:01 +0700

    Fix.

--- service/ocservreports.py
+++ service/ocservreports.py
@@ -219,8 +219,8 @@ def create_database_if_not_exists():
 def scheduled_task():
     """Scheduled task."""
     logger.info("scheduled_task: Start")
-    logger.info(f"Current mode: {'TEST' if TEST_MODE else 'PRODUCTION'}")
-    if TEST_MODE:
+    logger.info(f"Current mode: {'TEST_MODE'}")
+    if TEST_MODE.lower() == "true":
         schedule.every(30).seconds.do(create_report)
         schedule.every(30).seconds.do(create_report_mon)
     else:
