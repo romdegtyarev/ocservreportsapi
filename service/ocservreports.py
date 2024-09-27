45d9f2bf76926b92d45b673adbf840029ecd8c3f
Roman Degtyarev <guromka@gmail.com>
2024-09-27 12:52:58 +0700

    Fix.

--- service/ocservreports.py
+++ service/ocservreports.py
@@ -22,6 +22,7 @@ DB_USER = os.getenv('POSTGRES_USER')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 DB_NAME = os.getenv('POSTGRES_DB')
 TEST_MODE = os.getenv("TEST_MODE", "false")
+VPSFLAG = os.getenv('VPSFLAG')
 
 matplotlib_logger = logging.getLogger('matplotlib')
 matplotlib_logger.setLevel(logging.WARNING)
